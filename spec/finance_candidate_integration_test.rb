4013151aba484966bf3e9191f36350363865f2bd
timkellogg <tim.kellogg@gmail.com>
2015-06-14 16:57:03 -0700

    add more integration tests

---
+++ spec/finance_candidate_integration_test.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+require 'capybara/rspec'
+require './app'
+
+Capybara.app = Sinatra::Application 
+set :show_exceptions, false 
+
+describe 'the financial candidate search engine', {:type => :feature} do 
+	context 'with valid search terms' do 
+		it 'retrieves the correct result page' do 
+			visit '/finance/candidates'
+			expect(page).to have_content "Candidate's Spending Portal"							
+			expect(page).to have_content 'Web Design by Kellogg Web Studio' 
+			fill_in 'fec_id', :with => 'H6CA39020'		
+			select 'House', :from => 'Office'		
+			select 'CA', :from => 'State'					
+			select 'Republican', :from => 'Party'	
+			select 'True', :from 'is_incumbent'
+			click_button 'Search'											
+			expect(page).to have_content 'Search again?'					
+			expect(page).to have_content 'Your results were limited to:'	
+			expect(page).to have_css '.result-item'	
+			expect(page).to have_content 'Royce'	
+			expect(page).to_not have_css '#result-none'					
+		end
+	end
+
+	context 'with invalid search combo of office:house and re-election year' do 
+		it 'retrieves the results page correctly' do 
+			visit '/finance/candidates'
+			expect(page).to have_content 'Full Congressional Text Search'						
+			expect(page).to have_content 'Web Design by Kellogg Web Studio' 
+			select 'House', :from 'Office'
+			select '2018', :from 'term_class'
+			click_button 'Search'
+			expect(page).to have_content 'Search again?'					
+			expect(page).to have_no_content '.result_item:'	
+			expect(page).to have_css '#result-none'				
+		end
+	end
+end
---
+++ spec/finance_district_integration_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+require 'capybara/rspec'
+require './app'
+
+Capybara.app = Sinatra::Application 
+set :show_exceptions, false 
+
+describe 'the financial district search engine', {:type => :feature} do 
+	context 'with valid search terms' do 
+		it 'retrieves the correct result page' do 
+			visit '/finance/districts'
+			expect(page).to have_content "Districts' Spending Portal"							
+			expect(page).to have_content 'Web Design by Kellogg Web Studio' 	
+			select 'H', :from => 'office'		
+			select 'CA', :from => 'state'					
+			select 'Republican', :from => 'incumbent_party'
+			fill_in 'office_district', :with => 39
+			click_button 'Search'											
+			expect(page).to have_content 'Search again?'					
+			expect(page).to have_content 'Your results were limited to:'	
+			expect(page).to have_content 'Royce'	
+			expect(page).to_not have_css '#result-none'					
+		end
+	end
+
+	context 'with invalid search combo of office:house and re-election year' do 
+		it 'retrieves the results page correctly' do 
+			visit '/finance/districts'					
+			select 'H', :from => 'office'
+			select '2018', :from => 'term_class'
+			click_button 'Search'
+			expect(page).to have_content 'Search again?'					
+			expect(page).to have_no_content '.result_item:'	
+			expect(page).to have_css '#result-none'				
+		end
+	end
+end
