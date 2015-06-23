9a38221ddd8b7a817ff973f695bdf273c0142f7f
timkellogg <tim.kellogg@gmail.com>
2015-06-23 14:09:02 -0700

    filter on polls

--- public/js/search.js
+++ public/js/search.js
@@ -1,12 +1,23 @@
 $(document).ready(function() {
 
+	// when data is a list 
 	$('#search-term').keyup(function(event) {
-		$('ul').show();
+		$('ul').show(); // textsearch
 
 		var query = event.target.value;
 		var selector = "ul:contains(" + query + ")";
 
 		$('ul').not(selector).hide();
 	});
-	
+
+	// when data is tabular 
+	$('#search-term').keyup(function(event) {
+		$('table').show();
+
+		var query = event.target.value;
+		var selector = "table:contains(" + query + ")";
+
+		$('table').not(selector).hide();
+	});
+
 });	
