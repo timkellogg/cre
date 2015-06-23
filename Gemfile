ec0e80a564c0dfa90a5a7fbc886bd57a275bceb7
timkellogg <tim.kellogg@gmail.com>
2015-06-23 15:56:02 -0700

    fix http vs https loading for heroku

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'http://rubygems.org'
 ruby '2.2.1'
 
-# Production
+# Production/Development 
 gem 'rubygems-update'
 gem 'bundler'
 gem 'sinatra'
--- public/js/app.js
+++ public/js/app.js
@@ -62,4 +62,26 @@ $(document).ready(function() {
         $('#word-spin').text('Research');
     }, 3000 );
 
+    // Search/filtering 
+    
+    // when data is a list 
+    $('#search-term').keyup(function(event) {
+        $('ul').show(); // textsearch
+
+        var query = event.target.value;
+        var selector = "ul:contains(" + query + ")";
+
+        $('ul').not(selector).hide();
+    });
+
+    // when data is tabular 
+    $('#search-term').keyup(function(event) {
+        $('table').show();
+
+        var query = event.target.value;
+        var selector = "table:contains(" + query + ")";
+
+        $('table').not(selector).hide();
+    });
+
 });
