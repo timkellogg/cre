68c7756c27578d0d83d935c0a435d159982d38e5
timkellogg <tim.kellogg@gmail.com>
2015-06-23 13:30:09 -0700

    finish first cut of search script

--- public/js/search.js
+++ public/js/search.js
@@ -1,20 +1,13 @@
 $(document).ready(function() {
 
-	// solution: you should have the button labled "search" that applies to all pages
-	// you should have a input labled "search-term" that can apply to all pages 
+	$('#search-term').keyup(function(event) {
+		$('ul').show();
 
-	// finally, have an id of look-up/whatever that you assign to the DOM element you want to search
-	// this way, you only have one javascript file for all searches 
+		var query = event.target.value;
+		var selector = "ul:contains(" + query + ")";
 
-	$('#search-btn').on('click', function() {
+		$('ul').not(selector).hide();
 
-		var q = $('#search-term').val();
-
-		var first = $('.searchable')[0];
-		
-		console.log(first.innerHTML);
-		var q = "";
 	});
-
-
-});
+	
+});	
